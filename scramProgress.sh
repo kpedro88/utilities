da36b939f7d84931ca6ef65c4cbb715074c39e51
Kevin Pedro <kpedro88@gmail.com>
2016-08-03 15:28:00 -0600

    parallelize scramProgress

--- scramProgress.sh
+++ scramProgress.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-LOG=$1
+function getPkgStatus(){
+	PKG=$1
+	DIRLOG=$(grep $PKG $LOG)
+	if $(echo "$DIRLOG" | fgrep -q -m1 "built"); then
+		echo 2
+	elif $(echo "$DIRLOG" | fgrep -q -m1 "Compiling"); then
+		echo 1
+	else
+		echo 0
+	fi
+}
+export -f getPkgStatus
+
+export LOG=$1
 if [[ -z $LOG || ! -f $LOG ]]; then
   exit
 fi
 
-COUNTER=0
-FOUND=0
-INPROG=0
-
-for dir in `ls -d */*/`; do
-#  echo $dir
-  #remove trailing /
-  dir=$(echo $dir | rev | cut -c 2- | rev)
-  COUNTER=$(($COUNTER + 1))
-  DIRLOG=$(grep $dir $LOG)
-  if $(echo "$DIRLOG" | fgrep -q "built"); then
-    FOUND=$(($FOUND + 1))
-  elif $(echo "$DIRLOG" | fgrep -q "Compiling"); then
-    INPROG=$(($INPROG + 1))
-  fi
-done
-
 echo "scram status:"
-echo "in progress: $INPROG / $COUNTER"
-echo "   finished: $FOUND / $COUNTER"
+find . -maxdepth 2 -mindepth 2 -type d -wholename "*/*" -not -path '*/\.*' -printf '%P\0' | xargs -0 -n1 -P4 bash -c 'getPkgStatus "$@"' _ | awk 'BEGIN {ctr[0] = 0; ctr[1] = 0; ctr[2] = 0;} {ctr[$1]++;total++} END {collen=length(total); printf(" in progress: %*s / %*s\n",collen,ctr[1],collen,total); printf("    finished: %*s / %*s\n",collen,ctr[2],collen,total);}'
