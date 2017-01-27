fa88a67812f1da89f5c8f0d6508bdfe7baf2baea
Kevin Pedro <kpedro88@gmail.com>
2017-01-26 18:48:48 -0600

    use totals option for condor_q

--- condorq_all.sh
+++ condorq_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-condor_q -submitter pedrok | grep -F "completed" | awk '{ split($0,a," "); j1 += a[1]; j2 += a[3]; j3 += a[5]; j4 += a[7]; j5 += a[9]; j6 += a[11]; j7 += a[13];} END { print j1,"jobs;",j2,"completed,",j3,"removed,",j4,"idle,",j5,"running,",j6,"held,",j7,"suspended"}'
+condor_q -submitter pedrok -totals | grep -F "completed" | awk '{ split($0,a," "); j1 += a[1]; j2 += a[3]; j3 += a[5]; j4 += a[7]; j5 += a[9]; j6 += a[11]; j7 += a[13];} END { print j1,"jobs;",j2,"completed,",j3,"removed,",j4,"idle,",j5,"running,",j6,"held,",j7,"suspended"}'
 
