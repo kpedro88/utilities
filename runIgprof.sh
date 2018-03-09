4b1120fbea08a7c8588b5aef7ea6feb57625cadd
Kevin Pedro <kpedro88@gmail.com>
2018-03-09 15:18:55 -0600

    allow sorting without running a command

--- runIgprof.sh
+++ runIgprof.sh
@@ -7,7 +7,7 @@ sort_report() {
 	IGREP=igreport_${NAME}.res
 	IGSORT=igsorted_${NAME}_${MODULE}_${MODE}.res
 	if [ "$MODE" == "self" ]; then
-		awk -v module=${MODULE} 'BEGIN { total = 0; } { if(substr($0,0,1)=="["&&index($0,module)!=0) {print $0; total += $3;} } END { print "Total: "total } ' igreport_testNew.res | sort -n -r -k1 | awk '{ if(index($0,"Total: ")!=0){total=$0;} else{print $0;} } END { print total; }' > ${IGSORT} 2>&1
+		awk -v module=${MODULE} 'BEGIN { total = 0; } { if(substr($0,0,1)=="["&&index($0,module)!=0) {print $0; total += $3;} } END { print "Total: "total } ' igreport_testNew.res | sort -n -r -k3 | awk '{ if(index($0,"Total: ")!=0){total=$0;} else{print $0;} } END { print total; }' > ${IGSORT} 2>&1
 	elif [ "$MODE" == "desc" ]; then
 		awk -v module=${MODULE} 'BEGIN { total = 0; } { if(substr($0,0,1)=="-"){good = 0;}; if(good&&length($0)>0){print $0; total += $3;}; if(substr($0,0,1)=="["&&index($0,module)!=0) {good = 1;} } END { print "Total: "total } ' ${IGREP} | sort -n -r -k1 | awk '{ if(index($0,"Total: ")!=0){total=$0;} else{print $0;} } END { print total; }' > ${IGSORT} 2>&1		
 	fi
@@ -39,30 +39,33 @@ while getopts "c:n:t:s:d:r" opt; do
 	esac
 done
 
-if [ -z "$CMD" ]; then
-	echo "-c required"
+if [ -z "$CMD" ] && [ ${#SORTSELF[@]} -eq 0 ] && [ ${#SORTDESC[@]} -eq 0 ]; then
+	echo "-c or -s or -d required"
 	exit 1
 fi
 
-# special way to run a ROOT macro (otherwise difficult due to quote nesting)
-if [ -n "$ROOT" ]; then
-	CMD="root.exe -b -l -q $CMD"
+if [ -n "$CMD" ]; then
+	# special way to run a ROOT macro (otherwise difficult due to quote nesting)
+	if [ -n "$ROOT" ]; then
+		CMD="root.exe -b -l -q $CMD"
+	fi
+
+	IGNAME=igprof_${NAME}
+	IGREP=igreport_${NAME}.res
+	# subshell to log commands but avoid `set +x`
+	(set -x;
+	igprof -d $TARGET -pp -z -o ${IGNAME}.pp.gz ${CMD} > ${IGNAME}.log 2>&1;
+	igprof-analyse -d -v ${IGNAME}.pp.gz > ${IGREP} 2>&1;
+	)
+
+	echo "Produced ${IGREP}"
 fi
 
-IGNAME=igprof_${NAME}
-IGREP=igreport_${NAME}.res
-# subshell to log commands but avoid `set +x`
-(set -x;
-igprof -d $TARGET -pp -z -o ${IGNAME}.pp.gz ${CMD} > ${IGNAME}.log 2>&1;
-igprof-analyse -d -v ${IGNAME}.pp.gz > ${IGREP} 2>&1;
-)
-
-echo "Produced ${IGREP}"
-
-# find producers/filters/analyzers, make sorted list & total
+# find module contributions, make sorted list & total
 for MODULE in ${SORTSELF[@]}; do
 	sort_report self ${NAME} ${MODULE}
 done
+# find module descendants (e.g. producers/filters/analyzers descend from doEvent), make sorted list & total
 for MODULE in ${SORTDESC[@]}; do
 	sort_report desc ${NAME} ${MODULE}
 done
