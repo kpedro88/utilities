3b86fa81d17c7a75ecd5f439fb56b05c7ea3168a
Kevin Pedro <kpedro88@gmail.com>
2021-06-22 18:17:13 -0500

    fix for condor 9

--- condorq_all.sh
+++ condorq_all.sh
@@ -11,10 +11,13 @@ GREPCMD="completed"
 CONDORV=$(condor_q -version | grep CondorVersion | awk '{print $2}')
 IFS="." read -a CONDORVARRAY <<< "$CONDORV"
 
-if [ "${CONDORVARRAY[0]}" -ge 8 ] && [ "${CONDORVARRAY[1]}" -ge 6 ]; then
+FLAG="-submitter"
+if [ "${CONDORVARRAY[0]}" -ge 9 ]; then
+	FLAG=""
+fi
+if [ "${CONDORVARRAY[0]}" -gt 8 ] || ( [ "${CONDORVARRAY[0]}" -eq 8 ] && [ "${CONDORVARRAY[1]}" -ge 6 ] ); then
 	AWKCMD='{ split($0,a," "); j1 += a[4]; j2 += a[6]; j3 += a[8]; j4 += a[10]; j5 += a[12]; j6 += a[14]; j7 += a[16];} END { print j1,"jobs;",j2,"completed,",j3,"removed,",j4,"idle,",j5,"running,",j6,"held,",j7,"suspended"}'
 	GREPCMD="query"
 fi
 
-condor_q -submitter $USER -totals | grep -F "$GREPCMD" | grep -vF "all users" | awk "$AWKCMD"
-
+condor_q $FLAG $USER -totals | grep -F "$GREPCMD" | grep -vF "all users" | awk "$AWKCMD"
